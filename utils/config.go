c5bd95306b4368b087ca377b2f37ee08756c5c7a
Paolo Galeone <nessuno@nerdz.eu>
2014-03-31 20:50:52 +0200

    Connection string builder created

--- utils/config.go
+++ utils/config.go
@@ -5,22 +5,71 @@ import (
     "log"
     "io/ioutil"
     "errors"
+    "bytes"
+    "strconv"
 )
 
 type config struct {
     Username string
-    Password string
+    Password string // optional -> default: 
     DbName   string
-    SSLMode  string
+    Host     string // optional -> default: localhost
+    Port     int16  // optional -> default: 5432
+    SSLMode  string // optional -> default: disable
 }
 
-func parse(confPath string) (conf *config, e error) {
+func Parse(confPath string) (string, error) {
     log.Println("Parsing JSON config file " + confPath);
 
     contents, e := ioutil.ReadFile(confPath)
     if e != nil {
-        return nil, e
+        return "", e
     }
 
-    //WIP
+    var conf config
+    e = json.Unmarshal(contents, &conf)
+
+    if e != nil {
+        return "", e
+    }
+
+    if conf.Username == "" {
+        return "", errors.New("Postgresql doesn't support empty username")
+    }
+
+    if conf.DbName == "" {
+        return "", errors.New("Empty DbName field in " + confPath)
+    }
+
+    var ret bytes.Buffer
+
+    ret.WriteString("user=" + conf.Username + " dbname=" + conf.DbName+ " host=")
+
+    if conf.Host == "" {
+        ret.WriteString("localhost")
+    } else {
+        ret.WriteString(conf.Host)
+    }
+
+    if conf.Password != "" {
+        ret.WriteString(" password=" + conf.Password)
+    }
+
+    ret.WriteString(" sslmode=")
+
+    if conf.SSLMode == "" {
+        ret.WriteString("disable")
+    } else {
+        ret.WriteString(conf.SSLMode)
+    }
+
+    ret.WriteString(" port=")
+
+    if(conf.Port == 0) {
+        ret.WriteString("5432")
+    } else {
+        ret.WriteString(strconv.Itoa(int(conf.Port)))
+    }
+
+    return ret.String(), nil
 }

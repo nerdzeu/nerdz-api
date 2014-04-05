ef3c098a21a721e18381dc651103e9d3c7cb6ea4
Paolo Galeone <nessuno@nerdz.eu>
2014-04-05 14:39:57 +0200

    Improve data types, add funcionts

--- orm/user.go
+++ orm/user.go
@@ -2,13 +2,12 @@ package orm
 
 import (
 	"errors"
+	"net/mail"
 	"net/url"
 	"time"
 )
 
-// JSON
-
-type Info struct {
+type PersonalInfo struct {
 	Id       int64
 	IsOnline bool
 	Nation   string
@@ -18,11 +17,23 @@ type Info struct {
 	Surname  string
 	Gender   bool
 	Birthday time.Time
-	Gravatar url.URL
+	Gravatar *url.URL
 }
 
-func (user *User) New(id int64) error {
+type ContactInfo struct {
+	Email    *mail.Address
+	Website  *url.URL
+	GitHub   *url.URL
+	Skype    string
+	Jabber   string
+	Yahoo    *mail.Address
+	Facebook *url.URL
+	Twitter  *url.URL
+	Steam    string
+}
 
+// New initializes a User struct
+func (user *User) New(id int64) error {
 	db.First(user, id)
 	db.Find(&user.Profile, id)
 
@@ -33,9 +44,9 @@ func (user *User) New(id int64) error {
 	return nil
 }
 
-func (user *User) GetInfo() *Info {
-
-	return &Info{
+// GetInfo returns a PersonalInfo struct
+func (user *User) GetPersonalInfo() *PersonalInfo {
+	return &PersonalInfo{
 		Id:       user.Counter,
 		IsOnline: user.Viewonline && user.Last.Add(time.Duration(5)*time.Minute).After(time.Now()),
 		Nation:   user.Lang,
@@ -45,4 +56,35 @@ func (user *User) GetInfo() *Info {
 		Gender:   user.Gender,
 		Birthday: user.BirthDate,
 		Gravatar: getGravatar(user.Email)}
+}
+
+// GetContactInfo returns a ContactInfo struct
+func (user *User) GetContactInfo() *ContactInfo {
+	// Errors should never occurs, since values are stored in db after have been controlled
+	email, _ := mail.ParseAddress(user.Email)
+	yahoo, _ := mail.ParseAddress(user.Profile.Yahoo)
+	website, _ := url.Parse(user.Profile.Website)
+	github, _ := url.Parse(user.Profile.Github)
+	facebook, _ := url.Parse(user.Profile.Facebook)
+	twitter, _ := url.Parse(user.Profile.Twitter)
+
+	// Set Address.Name field
+	emailName := user.Surname + " " + user.Name
+	// email is always != nil, since an email is always required
+	email.Name = emailName
+	// yahoo address can be nil
+	if yahoo != nil {
+		yahoo.Name = emailName
+	}
+
+	return &ContactInfo{
+		Email:    email,
+		Website:  website,
+		GitHub:   github,
+		Skype:    user.Profile.Skype,
+		Jabber:   user.Profile.Jabber,
+		Yahoo:    yahoo,
+		Facebook: facebook,
+		Twitter:  twitter,
+		Steam:    user.Profile.Steam}
 }
--- orm/user_private.go
+++ orm/user_private.go
@@ -8,12 +8,12 @@ import (
 	"strings"
 )
 
-func getGravatar(email string) url.URL {
+func getGravatar(email string) *url.URL {
 
 	m := md5.New()
 	io.WriteString(m, strings.ToLower(email))
 
-	return url.URL{
+	return &url.URL{
 		Scheme: "https",
 		Host:   "www.gravatar.com",
 		Path:   "/avatar/" + fmt.Sprintf("%x", m.Sum(nil))}

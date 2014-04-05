4f5bba27f443d43b839e3a1d194a45527231b54d
Paolo Galeone <nessuno@nerdz.eu>
2014-04-05 14:39:21 +0200

    Uses of env var to pass configuration file path (required for tests)

--- orm/db.go
+++ orm/db.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/nerdzeu/gorm"
 	"github.com/nerdzeu/nerdz-api/utils"
+    "os"
 )
 
 var db gorm.DB
@@ -14,12 +15,19 @@ func init() {
 	flag.Parse()
 
 	args := flag.Args()
+    envVar := os.Getenv("CONF_FILE")
 
-	if len(args) != 1 {
-		panic(fmt.Sprintln("Configuration file is required"))
-	}
+    var file string
 
-	connStr, err := utils.Parse(args[0])
+	if len(args) == 1 {
+        file = args[0]
+	} else if envVar != "" {
+        file = envVar
+    } else {
+        panic(fmt.Sprintln("Configuration file is required.\nUse: CONF_FILE environment variable or cli args"))
+    }
+
+	connStr, err := utils.Parse(file)
 
 	if err != nil {
 		panic(fmt.Sprintf("[!] %v\n", err))

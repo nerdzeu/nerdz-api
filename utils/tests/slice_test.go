f2d21930eced306c8f03e2981ac5e9bf20298ee6
Paolo Galeone <nessuno@nerdz.eu>
2014-04-13 21:34:48 +0200

    Format test

--- utils/tests/slice_test.go
+++ utils/tests/slice_test.go
@@ -5,11 +5,10 @@ import (
 	"testing"
 )
 
-
 func TestValidSlice(t *testing.T) {
-    letters := []string{"a", "b", "c", "d"}
-    letters = utils.ReverseSlice(letters).([]string)
-    if letters[len(letters)-1] != "a" {
-        t.Errorf("Last letter should be 'a' but got: %v", letters[len(letters)-1])
-    }
+	letters := []string{"a", "b", "c", "d"}
+	letters = utils.ReverseSlice(letters).([]string)
+	if letters[len(letters)-1] != "a" {
+		t.Errorf("Last letter should be 'a' but got: %v", letters[len(letters)-1])
+	}
 }

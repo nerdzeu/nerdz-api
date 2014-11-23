6ae8e1ba1c5fcf46c45e3b56b4727a5b02036f7d
Paolo Galeone <nessuno@nerdz.eu>
2014-11-23 22:03:34 +0100

    Adding pm support

---
+++ nerdz/pm.go
@@ -0,0 +1,118 @@
+package nerdz
+
+import (
+	"errors"
+	"fmt"
+	"github.com/nerdzeu/nerdz-api/utils"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+// New initializes a Pm struct
+func NewPm(pmid uint64) (pm *Pm, e error) {
+	pm = new(Pm)
+	db.First(pm, pmid)
+
+	if pm.Pmid != pmid {
+		return nil, errors.New("Invalid pmid")
+	}
+
+	return pm, nil
+}
+
+// Implementing newMessage interface
+
+// Set the source of the pm (the user ID)
+func (pm *Pm) SetSender(id uint64) {
+	pm.From = id
+}
+
+// Set the destionation of the pm: user ID
+func (pm *Pm) SetReference(id uint64) {
+	pm.To = id
+}
+
+// SetText set the text of the message
+func (pm *Pm) SetText(message string) {
+	pm.Message = message
+}
+
+// SetLanguage set the language of the pm (useless)
+func (pm *Pm) SetLanguage(language string) error {
+	return nil
+}
+
+// ClearDefaults set to the go's default values the fields with default sql values
+func (pm *Pm) ClearDefaults() {
+	pm.Time = time.Time{}
+}
+
+// Implementing existingMessage interface
+
+// Id returns the User Post ID
+func (pm *Pm) Id() uint64 {
+	return pm.Pmid
+}
+
+// Lanaugage returns the message language
+func (pm *Pm) Language() (lang string) {
+	return nil
+}
+
+// NumericSender returns the id of the sender user
+func (pm *Pm) NumericSender() uint64 {
+	return pm.From
+}
+
+// From returns the sender *User
+func (pm *Pm) Sender() *User {
+	user, _ := NewUser(pm.NumericSender())
+	return user
+}
+
+// NumericReference returns the id of the recipient user
+func (pm *Pm) NumericReference() uint64 {
+	return pm.To
+}
+
+// To returns the recipient *User
+func (pm *Pm) Reference() Reference {
+	user, _ := NewUser(pm.NumericReference())
+	return user
+}
+
+// Message returns the pm message
+func (pm *Pm) Text() string {
+	return pm.Message
+}
+
+// IsEditable returns true if the pm is editable
+func (pm *Pm) IsEditable() bool {
+	return false
+}
+
+// NumericOwners returns a slice of ids of the owner of the pms (the ones that can perform actions)
+func (pm *Pm) NumericOwners() []uint64 {
+	return []uint64{pm.To, pm.From}
+}
+
+// Owners returns a slice of *User representing the users who own the pm
+func (pm *Pm) Owners() (ret []*User) {
+	return Users(pm.NumericOwners())
+}
+
+// Revisions returns all the revisions of the message
+func (pm *Pm) Revisions() (modifications []string) {
+	return
+}
+
+// RevisionNumber returns the number of the revisions
+func (pm *Pm) RevisionsNumber() uint8 {
+	return 0
+}
+
+// Thumbs returns the pm's thumbs value
+func (pm *Pm) Thumbs() int {
+	return 0
+}
